016d31d23c25426e811f59f2321eddb30c44d659
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-07-05 12:02:23 +0530

    ..

--- Conditional Statements/largestOfThreeNumbers.java
+++ Conditional Statements/largestOfThreeNumbers.java
@@ -1,8 +1,17 @@
+import java.util.*;
 public class largestOfThreeNumbers {
     public static void main(String[] args) {
-        int a = 100;
-        int b = 30;
-        int c = 10;
+        // int a = 100;
+        // int b = 30;
+        // int c = 10;
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the vale of a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b : ");
+        int b = sc.nextInt();
+        System.out.print("Enter the value of c : ");
+        int c = sc.nextInt();
+
     if (a >= b && a >= c){
         System.out.println("The larger number is a");
     }
--- Loops/whileLoop.java
+++ Loops/whileLoop.java
@@ -1,8 +1,8 @@
 public class whileLoop{
     public static void main(String[] args) {
         int counter = 0;
-        while (counter < 100){
-            System.out.println("Hello world");
+        while (counter <= 100){
+            System.out.println("2 * "+counter +" = " +2*counter);
             counter++;
         }
     }
