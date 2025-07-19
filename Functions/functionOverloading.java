fff2f8ee5d8027e93a76cc047f325fb4e96a24e8
Yogesh <yogeshmehra2509@gmail.com>
2025-07-19 23:33:19 +0530

    ..

--- Functions/functionOverloading.java
+++ Functions/functionOverloading.java
@@ -1,16 +1,34 @@
+// This is function overloading using Parameters 
+
+// public class functionOverloading {
+//     //function to print the sum of 2 numbers
+//     public static int sum(int a, int b){
+//         return a + b;
+//     }
+
+//     //function to print the sum of 3 numbers
+//     public static int sum(int a, int b, int c){
+//         return a + b + c;
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(sum(3 ,5));
+//         System.out.println(sum(5, 3, 1 ));
+//     }    
+// }
+
 public class functionOverloading {
-    //function to print the sum of 2 numbers
+    //function to calculate int sum
     public static int sum(int a, int b){
         return a + b;
     }
-
-    //function to print the sum of 3 numbers
-    public static int sum(int a, int b, int c){
-        return a + b + c;
+    //function to calculate float sum
+    public static float sum(float a, float b){
+        return a + b;
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(3 ,5));
-        System.out.println(sum(5, 3, 1 ));
-    }    
+        System.out.println(sum(4, 5));
+        System.out.println(sum(4.6f, 6.5f));
+    }
 }
--- Functions/primeNumbers.java
+++ Functions/primeNumbers.java
@@ -1,28 +1,41 @@
 public class primeNumbers {
-    // public static boolean isPrime(int n){
-    //     boolean isPrime = true;
-    //     for(int i=2; i<=n-1; i++){
-    //         if(n % i == 0){
-    //             isPrime = false;
-    //             break;
-    //         }
-    //     }
-    //     return isPrime;
-    // }
-
     public static boolean isPrime(int n){
+
+        //corner cases
+        //if the number is 2
+
         if(n == 2){
             return true;
         }
-        for(int i=2; i<=Math.sqrt(n); i++){
+        boolean isPrime = true;
+        for(int i=2; i<=n-1; i++){
             if(n % i == 0){
-                return false;
+                isPrime = false;
+                break;
             }
         }
-        return true;
+        return isPrime;
     }
+    
     public static void main(String[] args) {
-        System.out.println(isPrime(5));
-        
+        System.out.println(isPrime(112));
     }
 }
+
+// public class primeNumbers {
+//     public static boolean isPrime(int n){
+//         if(n == 2){
+//             return true;
+//         }
+//         for(int i=2; i<=Math.sqrt(n); i++){
+//             if(n % i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println(isPrime(5));
+        
+//     }
+// }
