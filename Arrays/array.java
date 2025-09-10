aa85f137d690fb1ea1751d004faa4d4f78f94e92
Yogesh <yogeshmehra2509@gmail.com>
2025-09-10 22:11:54 +0530

    ..

---
+++ Arrays/array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        
+        // Creating arrays
+
+        //int marks [] = new int [50];
+        int numbers [] = {1, 2, 3, 4, 5};
+        //int moreNumbers [] = {6, 7, 8, 9, 10};
+       // String fruits [] = {"Apple", "Banana", "Orange"};
+    }
+}
