90afd90f70112e19b3e99e8b24864a296fcb5555
Yogesh <yogeshmehra2509@gmail.com>
2025-09-10 23:34:35 +0530

    Array

--- Arrays/array.java
+++ Arrays/array.java
@@ -1,11 +1,29 @@
+import java.util.*;
 public class array {
     public static void main(String[] args) {
-        
-        // Creating arrays
 
-        //int marks [] = new int [50];
-        int numbers [] = {1, 2, 3, 4, 5};
-        //int moreNumbers [] = {6, 7, 8, 9, 10};
-       // String fruits [] = {"Apple", "Banana", "Orange"};
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+    // Input in Array
+    System.out.print("Enter the marks of Physics: ");
+        marks[0] = sc.nextInt();
+        System.out.print("Enter the marks of Chemistry: ");
+       marks[1] = sc.nextInt();
+       System.out.print("Enter tyhe marks of Maths:");
+       marks[2] = sc.nextInt();
+
+       //Output in Array
+         System.out.println("The marks of Physics is: " + marks[0]);
+        System.out.println("The marks of Chemistry is: " + marks[1]);
+        System.out.println("The marks of Maths is: " + marks[2]);
+
+        //Updating the marks
+        marks [2] = marks[2] + 2;
+        System.out.println("New Updated marks of Maths is :" + marks[2]);
+          
+          //Or
+          marks [1]= 99;
+          System.out.println("New Updated marks of Chemistry is :" + marks[1]);
     }
 }
