f781f7a9bebe672d29330d094ce17671e7da38a3
Yogesh <yogeshmehra2509@gmail.com>
2025-09-20 23:07:41 +0530

    ..

---
+++ Arrays/largestInArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class largestInArray {
+    
+    public static int getLargest (int numbers[]){
+        int largest = Integer.MIN_VALUE;     // -Infinity
+        for (int i = 0; i<numbers.length; i++){
+            if( largest < numbers[i]){
+                largest = numbers[i];
+            }
+        } return largest;
+    } 
+    public static void main(String args []){
+        int numbers[] = {1, 2, 6, 3, 5};
+        System.out.println("Largest Value is : "+getLargest(numbers));
+    }
+}
