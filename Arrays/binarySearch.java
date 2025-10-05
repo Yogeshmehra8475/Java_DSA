e9e6ed18cbf91eaa84ac941b09875bd70d906300
Yogesh <yogeshmehra2509@gmail.com>
2025-10-05 12:30:57 +0530

    ..

--- Arrays/binarySearch.java
+++ Arrays/binarySearch.java
@@ -1,5 +1,5 @@
 public class binarySearch {
-    public static int binarySearch(int numbers[],int key ){
+    public static int binarySearched(int numbers[],int key ){
         int start = 0, end = numbers.length - 1;
         while(start<= end){
             int mid = (start + end) / 2;
@@ -21,7 +21,7 @@ public class binarySearch {
     public static void main (String args []){
         int numbers[] = {2, 4, 6, 8, 10, 12, 14};
         int key = 10;
-        System.out.println("Index for key is : "+binarySearch( numbers, key));
+        System.out.println("Index for key is : "+binarySearched( numbers, key));
          
     }
 }
